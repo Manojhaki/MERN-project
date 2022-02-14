f6c41acabd37432a065be9e5639a9d8634624123
Manojhaki <manoj.haki@gmail.com>
2022-02-14 09:44:43 -0500

    adding 4 routes

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 
 //connect Database
 connectDB();
-app.get('/', (req,res) => res.send('API RUNNING'));
+app.get('/', (req, res) => res.send('API RUNNING'));
+
+//define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
