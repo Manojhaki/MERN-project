ec7efd252d8bc62cde0a2f62decff6d12696854b
Manojhaki <manoj.haki@gmail.com>
2022-02-16 18:48:56 -0500

    adding post to the user db

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@ route POST api/post
+//@desc create a comment
+//@acces Private 
+router.post('/', [auth, [
+    check('text','Text is required')
+        .not()
+        .isEmpty()
 
 
-//@ route GET api/post
-//@desc Test Route
-//@acces Public 
-router.get('/', (req, res) => res.send('Post Route'));
+]], async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        
+        const post = await newPost.save();
+        res.json(post);
+    
+} catch (error) {
+        console.error(error.message);
+        res.status(500).send('server Error')
+}
+    
+});
 
 module.exports = router;
