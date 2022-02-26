30984515aefbc3c1c1ee0a4ec48ff233fcb96d6f
Manojhaki <manoj.haki@gmail.com>
2022-02-26 12:46:09 -0500

    creating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
-const App=()=>  
+const App = () =>  {
+  <Router>
   <Fragment>
     <Navbar />
-    <Landing/>
-   </Fragment>
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Routes>
+          
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
 
+          
+      </Routes>
+      
+      </section> 
+    </Fragment>
+   
+    </Router>
+}
 
 export default App;
